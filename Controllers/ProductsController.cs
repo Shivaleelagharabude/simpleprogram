7091f4f0e449343dd4fd52d06930ccdef3aa8c29
Shivaleelagharabude <100184087+Shivaleelagharabude@users.noreply.github.com>
2022-02-22 11:52:24 +0530

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿
+using contosCastwebsite.Moduls;
+
+using contosCastwebsite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace contosCastwebsite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+          this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+        // [HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating )
+         
+        {
+            ProductService.AddRating(ProductId, Rating);
+
+            return Ok();
+        }
+
+       // public class RatingRequest
+        //{
+          //  public string ProductId { get; set; }
+          //  public int Rating { get; set; }
+       //}
+
+    }
+}
